68614523184e0682faaab9a55d6c89a1336ec3c8
Joe <joe2k01dev@gmail.com>
2017-03-16 17:16:16 +0100

    Added backup frame

--- src/joe2k01/pizzaBackup/Frame.java
+++ src/joe2k01/pizzaBackup/Frame.java
@@ -6,6 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.UIManager;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Frame extends JFrame {
 
@@ -31,12 +38,26 @@ public class Frame extends JFrame {
 	 * Create the frame.
 	 */
 	public Frame() {
+		setTitle("PizzaBackup by joe2k01");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 720, 480);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JButton btn_newBackup = new JButton("Create a new backup");
+		btn_newBackup.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				BackUpFrame.main(null);
+			}
+		});
+		contentPane.add(btn_newBackup, BorderLayout.SOUTH);
+		
+		Canvas mainBg = new Canvas();
+		mainBg.setBackground(UIManager.getColor("Button.background"));
+		contentPane.add(mainBg, BorderLayout.CENTER);
 	}
 
 }
